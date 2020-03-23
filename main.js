4a6afcdf87ec885955ac40f74269f33f64c5e69c
DIEZ91 <53362802+DIEZ91@users.noreply.github.com>
2020-03-24 00:12:31 +0300

    - refactoring

--- main.js
+++ main.js
@@ -1,136 +1,119 @@
-var colors = generateRandomColors(6);
-var pickedColor = pickColor();
-var squares = document.querySelectorAll('.square');
-var colorDisplay = document.getElementById('colorDisplay');
-var messageDisplay = document.getElementById('message');
-var headerColor = document.querySelector('h1');
-var resetButton = document.getElementById('resetButton');
-var gameMode = document.querySelectorAll('.mode');
-var hiddenDiv = document.querySelectorAll('.hide');
-var extraDiv = document.querySelectorAll('.extra');
-var scoreRight = 0;
-var scoreWrong = 0;
-var scoreRightDisplay = document.querySelector('#scoreRight');
-var scoreWrongDisplay = document.querySelector('#scoreWrong');
-var diff = 9;
+(() => {
+  let diff = 9;
+  let scoreRight = 0;
+  let scoreWrong = 0;
+  let colors = generateRandomColors(diff);
+  let pickedColor = pickColor();
+  let squares;
+  const colorDisplay = document.getElementById('colorDisplay');
+  const container = document.querySelector('.container');
+  const messageDisplay = document.getElementById('message');
+  const headerColor = document.querySelector('h1');
+  const resetButton = document.getElementById('resetButton');
+  const gameModeBtn = document.querySelectorAll('.mode');
+  const scoreRightDisplay = document.querySelector('#scoreRight');
+  const scoreWrongDisplay = document.querySelector('#scoreWrong');
 
-reset();
-
-
-for(var i = 0; i < gameMode.length; i++){
-  gameMode[i].addEventListener('click', function(){
-    gameMode[0].classList.remove('selected');
-    gameMode[1].classList.remove('selected');
-    gameMode[2].classList.remove('selected');
-    this.classList.add('selected');
-    if (this.textContent === 'EASY'){
-      diff = 3;
-      document.documentElement.style.fontSize = '17px';
-      for(var i = 0; i < 3; i++){
-        hiddenDiv[i].style.display = 'none';
-        extraDiv[i].style.display = 'none';
-      }
-    } else if (this.textContent === 'HARD') {
-        diff = 6;
-        document.documentElement.style.fontSize = '15px';
-      for(var i = 0; i < 3; i++){
-        hiddenDiv[i].style.display = 'block';
-        extraDiv[i].style.display = 'none';
-     };
-
-    } else if (this.textContent === 'BEAST'){
-      diff = 9;
-      document.documentElement.style.fontSize = '13px';
-      for(var i = 0; i < 3; i++){
-        extraDiv[i].style.display = 'block';
-        hiddenDiv[i].style.display = 'block';
-      };
-    }
-    reset();
-  })
-}
-
-
-
-function reset(){
-  //generate new colors
-  colors = generateRandomColors(diff);
-  //new pick colors
-  pickedColor = pickColor();
-  //color display
-  colorDisplay.textContent = pickedColor;
-  //change colors of squares
-  for(var i = 0; i < diff; i++){
-    //add colors to squares
-    squares[i].style.backgroundColor = colors[i];
-    //change text back
-    resetButton.textContent = 'New Colors';
-    //reset header colors
-    headerColor.style.backgroundColor = 'white';
-    //message reset
-    messageDisplay.textContent = "Let's try...";
-    messageDisplay.classList.remove('green');
-    messageDisplay.classList.remove('red');
-  };
-};
-
-resetButton.addEventListener('click', function(){
+  generateBlocks(diff);
   reset();
-})
 
-
-for(var i = 0; i < diff; i++){
-  //add colors to squares
-  squares[i].style.backgroundColor = colors[i];
-
-  // add event listener
-  squares[i].addEventListener('click', function(){
-  // grab colors
-  var clickedColor = this.style.backgroundColor;
-  //compare color
-  if (clickedColor === pickedColor){
-    messageDisplay.textContent = 'CORRECT!';
-    changeColor(pickedColor);
-    messageDisplay.classList.remove('red');
-    messageDisplay.classList.add('green');
-    headerColor.style.backgroundColor = pickedColor;
-    resetButton.textContent = 'Play Again?';
-    scoreRight++;
-    scoreRightDisplay.textContent = scoreRight;
-  } else {
-    this.style.backgroundColor = 'white';
-    messageDisplay.textContent = 'WRONG!';
-    messageDisplay.classList.add('red');
-    scoreWrong++;
-    scoreWrongDisplay.textContent = scoreWrong;
-  }
+  gameModeBtn.forEach( el => {
+    el.addEventListener('click', event => {
+      gameModeBtn.forEach(el => {
+        el.classList.remove('selected');
+      })
+      event.target.classList.add('selected');
+      if (event.target.textContent === 'EASY'){
+        diff = 3;
+        container.innerHTML = '';
+        generateBlocks(diff);
+      } else if (event.target.textContent === 'HARD') {
+          diff = 6;
+          container.innerHTML = '';
+          generateBlocks(diff);
+      } else if (event.target.textContent === 'BEAST'){
+          diff = 9;
+          document.documentElement.style.fontSize = '13px';
+          generateBlocks(diff);
+      }
+      reset();
+    })
   })
-}
 
-
-function changeColor (color){
-  for(var i = 0; i < diff; i++){
-    squares[i].style.backgroundColor = color;
+  function generateBlocks (num) {
+    const div = `<div class="square"></div>`;
+    for(let i = 0; i < num; i++){
+      document.querySelector('.container').insertAdjacentHTML('beforeend', div)
+    }
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square, i) => {
+      square.style.backgroundColor = colors[i];
+      square.addEventListener('click', ev => {
+        // grab colors
+        let clickedColor = ev.target.style.backgroundColor;
+        //compare color
+        if (clickedColor === pickedColor){
+          messageDisplay.textContent = 'CORRECT!';
+          changeColor(pickedColor);
+          messageDisplay.classList.remove('red');
+          messageDisplay.classList.add('green');
+          headerColor.style.backgroundColor = pickedColor;
+          resetButton.textContent = 'Play Again?';
+          scoreRight++;
+          scoreRightDisplay.textContent = scoreRight;
+        } else {
+          if(ev.target.style.backgroundColor !== 'white'){
+            scoreWrong++;
+          }
+          ev.target.style.backgroundColor = 'white';
+          messageDisplay.textContent = 'WRONG!';
+          messageDisplay.classList.add('red');
+          scoreWrongDisplay.textContent = scoreWrong;
+        }
+        })
+    })
   }
-}
 
+  function reset(){
+    colors = generateRandomColors(diff);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    squares.forEach((square, i) => {
+      square.style.backgroundColor = colors[i];
+    });
+    resetButton.textContent = 'New Colors';
+    headerColor.style.backgroundColor = 'white';
+    messageDisplay.textContent = "Let's try...";
+    messageDisplay.classList.remove('green','red');
+  };
 
-function pickColor(){
-  var random = Math.floor(Math.random() * colors.length);
-  return colors[random];
-}
-
-function generateRandomColors(num){
-  //make an array
-  var arr = [];
-  //put random colorDisplay
-  for(i = 0; i < num; i++){
-    var r = Math.floor(Math.random() * 256);
-    var g = Math.floor(Math.random() * 256);
-    var b = Math.floor(Math.random() * 256);
-
-    arr.push('rgb(' + r + ',' + ' ' + g + ',' + ' ' + b + ')');
+  function changeColor (color){
+    for(var i = 0; i < diff; i++){
+      squares[i].style.backgroundColor = color;
+    }
   }
-  //return the array
-  return arr;
-}
+
+
+  function pickColor(){
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+  };
+
+  function generateRandomColors(num){
+    let arrColors = Array.apply(null, Array(num)).map(el => {
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      return `rgb(${r}, ${g}, ${b})`
+    });
+    return arrColors; 
+  };
+
+  resetButton.addEventListener('click', function(){
+    reset();
+  });
+})();
+
+
+
+
